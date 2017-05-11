5cc3c0ee5c17356a590c855b1ace1a97f6c5d866
ruslegg <negrei.ruslan@gmail.com>
2017-05-11 13:17:06 +0200

    Initial Commit

---
+++ DreamLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Ruslan Negrei on 5/11/17.
+//  Copyright © 2017 Ruslan Negrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
